f876164ac8d783168b5b763f66128c2a37b23184
chiwent <atsimonchen@163.com>
2019-02-16 17:43:10 +0800

    修改目标站点为dysfz.vip

--- bin/index.js
+++ bin/index.js
@@ -17,13 +17,13 @@ Usage
 Options
     --name, -n 资源名称，可以使用关键词
 `, {
-    flags: {
-        name: {
-            type: 'string',
-            alias: 'n'
+        flags: {
+            name: {
+                type: 'string',
+                alias: 'n'
+            }
         }
-    }
-});
+    });
 
 
 if (!cli.flags.name) {
@@ -31,7 +31,7 @@ if (!cli.flags.name) {
     process.exit(0);
 }
 
-let base_url = 'http://www.dysfz.cc/key/'
+let base_url = 'http://www.dysfz.vip/key/'
 
 const search = new Search(cli.flags);
 
@@ -40,33 +40,18 @@ let url = encodeURI(base_url + cli.flags.name + '/');
 const headers = search.headers;
 
 
-let select = (json) => {
-    return new Promise((resolve, reject) => {
-        let data = json.pop(0).href;
-        let rl = readline.createInterface({
-            input: process.stdin,
-            output: process.stdout
-        });
-        rl.question('请选择序号:  ', (answer) => {
-            console.log('请稍等...');
-            rl.close();
-        })
-        resolve(data)
-    })
-
-}
-let asyncGet = async function(json) {
-    let data = json.pop(0).href;
+let asyncGet = async function (res) {
     let rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout
     });
     await rl.question('请选择序号:  ', (answer) => {
-        console.log('请稍等...');
+        console.log(res[answer - 1].href)
+        console.log('你选择了第' + answer + '项，请稍等...');
         rl.close();
         request({
-            url: data,
-            headers: headers
+            url: res[answer - 1].href,
+            // headers: headers
         }, (err, res, body) => {
             let $ = cheerio.load(body);
             let baidu_title = $('div.detail hr').first().next().next().text();
@@ -75,7 +60,7 @@ let asyncGet = async function(json) {
             console.log(chalk.cyan(baidu_title));
             console.log(chalk.cyan(baidu_href + '\n'));
             let result = [];
-            xunlei.each(function(i, elem) {
+            xunlei.each(function (i, elem) {
                 result.push({
                     title: $(this).text(),
                     href: $(this).attr('href')
@@ -92,13 +77,12 @@ let asyncGet = async function(json) {
 
 search.searchItem(url)
     .then(result => {
-        let json = JSON.parse(result).html;
         console.log(chalk.bold.blue('搜索到的资源如下，请选择你想要的资源的序号'));
-        json.forEach((item, index) => {
+        result.forEach((item, index) => {
             console.log(chalk.cyan(`${index + 1} -  ${item.title}`));
             console.log(chalk.cyan(`网页链接: ${item.href}`));
         });
-        return json;
-    }).then(json => {
-        asyncGet(json)
+        return result;
+    }).then(result => {
+        asyncGet(result)
     })
